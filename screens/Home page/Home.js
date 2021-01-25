4e2252f7e60580268b33a71b6f139840bfc6b5c9
DevPopat <popatd@uci.edu>
2021-01-25 10:59:24 +0530

    chores: porting from private repo

---
+++ screens/Home page/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function Home({ navigation }) {
+  /*const pressHandler = () =>{
+       navigation.navigate('BarCodeScanner');
+   }*/
+
+  return (
+    <Button
+      title="Open Scanner"
+      //onPress = {pressHandler}
+    />
+  );
+}
